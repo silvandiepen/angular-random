a514609555c7b7c686a205a513202a9a98325915
silvandiepen <me@silvandiepen.com>
2016-12-21 13:48:27 +0100

    updated

--- angular-random.js
+++ angular-random.js
@@ -1,6 +1,13 @@
-/* global angular */
-angular.module('random')
-.factory('random', [function() {
+
+/*!
+ * angular-random.js v0.0.1 - A standalone AngularJS implementation to get a random number with a min and max value
+ * Copyright (c) 2016 Sil van Diepen - http://github.com/silvandiepen/angular-random
+ * License: MIT
+*/
+
+(function(window, angular, undefined) {
+	angular.module('random')
+	.service('random', [function() {
     'use strict';
 
     var self = this;
@@ -10,6 +17,7 @@ angular.module('random')
         max = max === null? MAX_INT : max;
         return min + (max - min) * Math.rand();
     };
-
     return self.Random;
-}]);
+
+	}]);
+})(window, window.angular);
